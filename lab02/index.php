15d5c9da6970334a9164cb51783350a98eb5d5bf
sta5on <stan.voronet@gmail.com>
2026-02-13 08:37:28 +0200

    add lab 2

---
+++ lab02/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Привет, мир!<br />";
+
+echo "Hello, World with echo!<br />";
+print "Hello, World with print!<br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Конкатенация (через .): " . $days . " дней. " . $message . "<br />";
+
+echo "Двойные кавычки: Через $days дней $message<br />";
+
+echo "Пока";
